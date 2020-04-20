042642a391e6459ddccf433d3c2823671810edf3
liambloom <liamrbloom@gmail.com>
2020-04-20 18:20:22 -0400

    Deleted old files

--- Test/Main.java
+++
@@ -1,9 +0,0 @@
-import java.util.Random;
-
-public class Main {
-  public static Random r = new Random();
-  public static void main (String[] args) {
-    int i;
-    while ((i = r.nextInt(1000)) <= 900) System.out.println(i);
-  }
-}
