6a5b5677884df7a5ec04b9c930f13d35e067604e
liambloom <liamrbloom@gmail.com>
2020-04-21 13:37:05 -0400

    indentation

--- src/magic8/Main.js
+++ src/magic8/Main.js
@@ -2,26 +2,26 @@
 // Keep in mind there is no html here, EVERYTHING is in js
 
 const responses = [
-  "It is certain",
-  "It is decidedly so",
-  "Without a doubt",
-  "Yes, definitely",
-  "You may rely on it",
-  "As I see it, yes",
-  "Most likely",
-  "Outlook good",
-  "Yes",
-  "Signs point to yes",
-  "Reply hazy, try again",
-  "Ask again later",
-  "Better not tell you now",
-  "Cannot predict now",
-  "Concentrate and ask again",
-  "Don't count on it",
-  "My reply is no",
-  "My sources say no",
-  "Outlook not so good",
-  "Very doubtful"
+      "It is certain",
+      "It is decidedly so",
+      "Without a doubt",
+      "Yes, definitely",
+      "You may rely on it",
+      "As I see it, yes",
+      "Most likely",
+      "Outlook good",
+      "Yes",
+      "Signs point to yes",
+      "Reply hazy, try again",
+      "Ask again later",
+      "Better not tell you now",
+      "Cannot predict now",
+      "Concentrate and ask again",
+      "Don't count on it",
+      "My reply is no",
+      "My sources say no",
+      "Outlook not so good",
+      "Very doubtful"
 ];
 
 document.body.style.setProperty("display", "flex");
