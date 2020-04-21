515a49916b8a4b669eed0086deaf274aec138473
liambloom <liamrbloom@gmail.com>
2020-04-20 22:05:47 -0400

    Chapter 7 stuff

---
+++ src/liam/chapter7/SelfChecks.java
@@ -0,0 +1,9 @@
+package liam.chapter7;
+
+public class SelfChecks {
+    public static void main(String[] args) {
+    }
+    public static void sc3 () {
+        int[] data = {2};
+    }
+}
--- src/liam/chapter7/Test.java
+++ src/liam/chapter7/Test.java
@@ -1,12 +1,15 @@
 package liam.chapter7;
 
+import java.util.Arrays;
+
 public class Test {
     public static void main (String[] args) {
         int[] ints = {1, 2};
         incrementAll(ints);
-        System.out.println(Main.arrayToString(ints));
+        System.out.println(Arrays.toString(ints));
     }
     public static void incrementAll (int[] ints) {
         for (int i = 0; i < ints.length; i++) ints[i]++;
     }
+
 }
