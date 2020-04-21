6c7bfbb342529bd076417a7096dbe7fa78a5f767
liambloom <liamrbloom@gmail.com>
2020-04-21 01:29:56 -0400

    Did up to 7.2

--- src/liam/chapter7/SelfChecks.java
+++ src/liam/chapter7/SelfChecks.java
@@ -1,9 +1,39 @@
 package liam.chapter7;
 
-public class SelfChecks {
+//import liam.chapter4.Ask;
+import java.util.Arrays;
+
+public class SelfChecks { // These were created to check my answers, but I did the problem before writing this.
     public static void main(String[] args) {
+        //System.out.println(Arrays.toString(sc4(Ask.forInt("min"), Ask.forInt("max"))));
+        sc14();
     }
     public static void sc3 () {
-        int[] data = {2};
+        int[] data = /*new int[]*/{2};
+    }
+    public static int[] sc4 (int min, int max) {
+        int[] r = new int[max - min];
+        for (int i = 0; i < r.length; i++) r[i] = i + min;
+        return r;
+    }
+    public static void sc5 () {
+        int[] data = new int[8];
+        data[0] = 3;
+        data[7] = -18;
+        data[4] = 5;
+        data[1] = data[0];
+
+        int x = data[4];
+        data[4] = 6;
+        data[x] = data[0] * data[1];
+
+        System.out.println(Arrays.toString(data));
+    }
+    public static void sc14 () {
+        int[] list = {2, 18, 6, -4, 5, 1};
+        for (int i = 0; i < list.length; i++) {
+            list[i] = list[i] + (list[i] / list[0]);
+        }
+        System.out.println(Arrays.toString(list));
     }
 }
--- src/liam/chapter7/Test.java
+++ src/liam/chapter7/Test.java
@@ -4,12 +4,18 @@ import java.util.Arrays;
 
 public class Test {
     public static void main (String[] args) {
-        int[] ints = {1, 2};
+        //sortTest();
+        System.out.println(Arrays.toString(Arrays.copyOf(new int[]{1, 2}, 2)));
+        /*int[] ints = {1, 2};
         incrementAll(ints);
-        System.out.println(Arrays.toString(ints));
+        System.out.println(Arrays.toString(ints));*/
     }
     public static void incrementAll (int[] ints) {
-        for (int i = 0; i < ints.length; i++) ints[i]++;
+        for (int i : ints) i++;
     }
-
+    public static void sortTest () {
+        int[] arr = {1, 3, 2};
+        Arrays.sort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
 }
