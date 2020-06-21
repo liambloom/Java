6111574b454fbd9b8a19625fce9b571e960a282c
liambloom <liamrbloom@gmail.com>
2020-06-21 01:09:29 -0400

    Made ArrayList$ have instance methods
    It now contains an ArrayList instead of havaing it as an argument

--- src/liam/chapter10/Exercises.java
+++ src/liam/chapter10/Exercises.java
@@ -1,13 +1,11 @@
 package liam.chapter10;
 
-
-import java.util.Arrays;
-import java.util.ArrayList;
+import java.util.*;
 
 public class Exercises {
     public static void main (String[] args) {
-        ArrayList<Integer> a = new ArrayList<>(Arrays.asList(1, 1, 3, 0, -3, -3));
-        exercise7(a);
+        ArrayList<String> a = new ArrayList<>(Arrays.asList("one", "two", "three", "four", "five", "six", "seven", "eight"));
+        exercise14(a);
         System.out.println(a);
     }
     public static double exercise1 (final ArrayList<String> strs) { // Avg # of vowels
@@ -23,21 +21,18 @@ public class Exercises {
         return liam.chapter7.$.avg(vowels.toArray(new Integer[0]));
     }
     public static <T> void exercise2 (ArrayList<T> a) { // Switch each pair of elements
-        for (int i = 0; i < a.size() - 1; i += 2) {
-            a.add(i + 2, a.get(i));
-            a.remove(i);
-        }
+        new ArrayList$<>(a).reverseSetsOf(2);
     }
     public static void exercise3 (ArrayList<String> a) { // Remove strings of even length
         a.removeIf(e -> e.length() % 2 == 0);
     }
     public static <T> void exercise4 (ArrayList<T> a) { // Duplicate every element
-        $.forEachReverse(a, (e, i) -> a.add(i, e));
+        new ArrayList$<>(a).forEachReverse((e, i) -> a.add(i, e));
     }
     public static <T extends Comparable<Integer>> void exercise5 (ArrayList<T> a) { // Replace every element n with n copies of itself
         // I don't like that different number types are not comparable to each other
         a.removeIf(e -> e.compareTo(0) <= 0);
-        $.forEachReverse(a, (e, i) -> {
+        new ArrayList$<>(a).forEachReverse((e, i) -> {
             for (int j = 1; e.compareTo(j) > 0; j++) {
                 a.add(i, e);
             }
@@ -53,7 +48,7 @@ public class Exercises {
         a.add(0, min);
     }
     public static <T> void exercise7 (ArrayList<T> a) { // Remove duplicates (assumes that a is sorted)
-        $.iterate(a.listIterator(), a::removeIf, (e, i) -> i != a.indexOf(e));
+        new ArrayList$<>(a).iterate(a::removeIf, (e, i) -> i != a.indexOf(e));
     }
 
 }
