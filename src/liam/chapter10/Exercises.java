8f3f1d6c12f327f3a200d0c7c6f5d7f0cded2d28
liambloom <liamrbloom@gmail.com>
2020-06-21 01:09:39 -0400

    Update Exercises.java

--- src/liam/chapter10/Exercises.java
+++ src/liam/chapter10/Exercises.java
@@ -50,6 +50,35 @@ public class Exercises {
     public static <T> void exercise7 (ArrayList<T> a) { // Remove duplicates (assumes that a is sorted)
         new ArrayList$<>(a).iterate(a::removeIf, (e, i) -> i != a.indexOf(e));
     }
-
+    public static <T extends Comparable<Integer>> void exercise8 (ArrayList<T> a) { // Removes 0s from array
+        a.removeIf(e -> e.compareTo(0) == 0);
+    }
+    public static <T extends Comparable<Integer>> int exercise9 (ArrayList<T> a) { // Returns the largest distance between 2 zeros
+        final int i0 = a.indexOf(0);
+        final int li0 = a.lastIndexOf(0);
+        return a.contains(0) && i0 == li0 ? 1 : li0 - i0;
+    }
+    public static <T extends Comparable<T>> void exercise10 (ArrayList<T> a, T start, T end) { // Remove everything between values start and end
+        a.removeIf(e -> start.compareTo(e) < 0 && e.compareTo(end) < 0);
+    }
+    public static <T> void exercise11 (ArrayList<T> a, int k) { // Replaces every element with k copies of itself
+        if (k <= 0) a.removeIf(_e -> true);
+        else {
+            new ArrayList$<>(a).forEachReverse((e, i) -> {
+                for (int j = 1; j < k; j++) a.add(i, e);
+            });
+        }
+    }
+    public static void exercise12 (ArrayList<String> a) { // Adds "****" before each four character string
+        new ArrayList$<>(a).forEachReverse((e, i) -> {
+            if (e.length() == 4) a.add(i, "****");
+        });
+    }
+    public static <T> void exercise13 (ArrayList<T> a) { // Same as exercise 2 but with sets of 3
+        new ArrayList$<>(a).reverseSetsOf(3);
+    }
+    public static void exercise14 (ArrayList<String> a) { // Removes the shorter of each pair of strings
+        for (int i = 0; i < a.size(); i++) a.remove(a.get(i).length() > a.get(i + 1).length() ? i + 1 : i);
+    }
 }
 
