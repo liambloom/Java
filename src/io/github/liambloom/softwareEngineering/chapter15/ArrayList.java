1ceef4879340e9ef1ce417311ffef4ebe4f57e84
liambloom <liamrbloom@gmail.com>
2020-09-07 19:51:44 -0400

    Did chapter 15 exercises 1-7

--- src/io/github/liambloom/softwareEngineering/chapter15/ArrayList.java
+++ src/io/github/liambloom/softwareEngineering/chapter15/ArrayList.java
@@ -1,16 +1,15 @@
 package io.github.liambloom.softwareEngineering.chapter15;
 
+import io.github.liambloom.softwareEngineering.chapter7.$;
 import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.Arrays;
 import java.util.Iterator;
-
 /**
  * This is a clone of {@code java.util.ArrayList<E>}.
  * 
  * @author Liam Bloom 
  */
-
 @SuppressWarnings("unchecked")
 public class ArrayList<E> implements Iterable<E> {
 
@@ -20,13 +19,23 @@ public class ArrayList<E> implements Iterable<E> {
      * @param args args
      */
     public static void main(String[] args) {
-        ArrayList<Integer> x = new ArrayList<>();
+        /*java.util.ArrayList<Foo> x = new java.util.ArrayList<>();
+        Foo y = new Foo();
+        //x.repla
+        x.add(y);
+        x.add(y);
+        x.get(0).increment();
+        System.out.println(x.get(1));*/
+        ArrayList<Integer> x = new ArrayList<>(new Integer[]{ 7, 42, 308, 409, 19, 17, 2 });
+        //ArrayList<String> x = new ArrayList<>(new String[]{"foo", "bar"});
+        //x.reverse();
+        /*x.add(1);
         x.add(1);
-        x.add(2);
-        x.add(3);
-        x.add(1, 4);
+        //x.add(3);
+        //x.add(1, 4);
         //x.remove(4);
-        System.out.println(x);
+        x.replaceAll(1, 2);*/
+        System.out.println(x.isPairwiseSorted());
     }
 
     /**
@@ -249,21 +258,65 @@ public class ArrayList<E> implements Iterable<E> {
     }
 
     /**
-     * Replaces an element in the list with another element
+     * Exercise 6. Fills the list with a particular value. <strong>Warning:</strong>
+     * this does not clone of newValue. If there are multiple matches, they will all
+     * be replaced by pointers to <em>the same object</em>, not copies of it.
+     * 
+     * @param value the value to fill the list with
+     */
+    public void fill(E value) {
+        for (int i = 0; i < size; i++) elementData[i] = value;
+    }
+
+    /**
+     * Replaces an element in the list with another element. <strong>Warning:</strong>
+     * this does not clone of newValue. If there are multiple matches, they will all 
+     * be replaced by pointers to <em>the same object</em>, not copies of it.
+     * 
+     * @param oldValue The value to be replaced
+     * @param newValue The value to replace it with
+     * @param warn If {@code true}, a warning will be printed to {@code System.err} if
+     *              multiple pointers to {@code newValue} are added.
+     */
+    public void replaceAll(E oldValue, E newValue, boolean warn) {
+        int i;
+        int changed = 0;
+        while ((i = indexOf(oldValue)) != -1) {
+            /*if (warn && ++changed == 2 && !Cloneable.class.isAssignableFrom(newValue.getClass())) 
+                System.err.printf(
+                    "\u001b[33mWarning:\u001b[0m This list now has two references to the object %s. These are not just equal, they are \u001b[1mthe same object\u001b[0m.%n",
+                    newValue.toString(), newValue.getClass().getSimpleName());*/
+            set(i, newValue);
+        }
+    }
+
+    /**
+     * Exercise 3. Replaces an element in the list with another element. Same as 
+     * {@link #replaceAll(Object, Object, boolean)} but the last argument is set to true
      * 
      * @param oldValue The value to be replaced
      * @param newValue The value to replace it with
      */
     public void replaceAll(E oldValue, E newValue) {
-        int i;
-        while ((i = indexOf(oldValue)) != -1) set(i, newValue);
+        replaceAll(oldValue, newValue, true);
     }
 
     /**
-     * Returns the index of the given value
+     * Exercise 4. Reverses the order of the list
+     */
+    public void reverse() {
+        for (int i = 0; i < size / 2; i++) {
+            E temp = elementData[i];
+            elementData[i] = elementData[size - i - 1];
+            elementData[size - i - 1] = temp;
+        }
+    }
+
+    /**
+     * Returns the index of the first occurrence of given value
      * 
      * @param value the value that will be searched for
-     * @return The index of the given value. {@code -1} if the value is not found
+     * @return the index of the given value. {@code -1} if the value is not found
      */
     public int indexOf(E value) {
         for (int i = 0; i < size; i++) {
@@ -273,7 +326,38 @@ public class ArrayList<E> implements Iterable<E> {
     }
 
     /**
-     * Returns the smallest value in the list
+     * Exercise 1. Returns the index of the last occurrence of given value
+     * 
+     * @param value the value that will be searched for
+     * @return the index of the last occurrence given value. {@code -1} if the value is not found
+     */
+    public int lastIndexOf(E value) {
+        for (int i = size - 1; i >= 0; i++) {
+            if (elementData[i].equals(value)) return i;
+        }
+        return -1;
+    }
+
+    /**
+     * Exercise 2. Returns the index of the first occurrence of an ordered list of values.
+     * I personally think that this should have been an overload of the {@link #indexOf(Object)}
+     * method, but I decided to just go with what the book said.
+     * 
+     * @param l the list that will be searched for
+     * @return the index of the last occurrence given value. {@code -1} if the value is not found
+     */
+    public int indexOfSubList(ArrayList<E> l) {
+        outer: for (int i = 0; i < size; i++) {
+            for (int j = 0; j < l.size; j++) {
+                if (!l.get(j).equals(get(i + j))) continue outer;
+            }
+            return i;
+        }
+        return -1;
+    }
+
+    /**
+     * Self Check 8. Returns the smallest value in the list
      * 
      * @return the smallest value in the list
      * @throws IllegalStateException if the list is empty
@@ -291,7 +375,7 @@ public class ArrayList<E> implements Iterable<E> {
     }
 
     /**
-     * Returns the largest value in the list
+     * Self Check 8. Returns the largest value in the list
      * 
      * @return the largest value in the list
      * @throws IllegalStateException if the list is empty
@@ -305,6 +389,66 @@ public class ArrayList<E> implements Iterable<E> {
             if (((Comparable<E>) max).compareTo(e) < 0) max = e;
         }
         return max;
+    }
+
+    /**
+     * Exercise 7. Checks if the list is pairwise sorted. &ldquo;A list is
+     * considered pairwise sorted if each successive pair of numbers is in
+     * nondecreasing order&rdquo; (Stuart Reges and Marty Stepp, Building Java
+     * programs: a back to basics approach&mdash;Third Edition).
+     * 
+     * @return A boolean representing weather this list is pairwise sorted
+     * @throws ClassCastException if {@code E} does not implement {@code Comparable<E>}
+     */
+    public boolean isPairwiseSorted() {
+        for (int i = 0; i < size / 2 * 2; i += 2) {
+            if (((Comparable<E>) elementData[i]).compareTo(elementData[i + 1]) > 0) return false;
+        }
+        return true;
+    }
+
+    /**
+     * Self Check 19. Returns the sum of the elements.
+     * 
+     * @return the sum of every element in the list
+     * @throws ClassCastException if any of the elements is a different type from
+     *                            another or if they are not one of Byte, Short,
+     *                            Integer, Long, Float, or Double
+     */
+    public E sum() {
+        if (size == 0) return (E) Integer.valueOf(0);
+        else return (E) $.castNumber($.sum(asDoubleArr()), (Class<? extends Number>) elementData[0].getClass());
+    }
+
+    /**
+     * Self Check 20. Returns the average of the elements
+     * 
+     * @return the average of every element
+     * @throws ClassCastException if any of the elements is a different type from
+     *                            another or if they are not one of Byte, Short,
+     *                            Integer, Long, Float, or Double
+     */
+    public E average() {
+        if (size == 0) return (E) Integer.valueOf(0);
+        else return (E) $.castNumber(((Number) sum()).doubleValue() / size, (Class<? extends Number>) elementData[0].getClass());
+    }
+
+    /**
+     * Exercise 5. Returns a new {@code ArrayList<E>} where the nth value of the new
+     * list is the sum of elements 0 through n of this list
+     * 
+     * @return a list of the cumulative sums at every index of this list
+     * @throws ClassCastException if any of the elements is a different type from
+     *                            another or if they are not one of Byte, Short,
+     *                            Integer, Long, Float, or Double
+     */
+    public ArrayList<E> runningTotal() {
+        double[] arr = asDoubleArr();
+        for (int i = 1; i < size; i++) arr[i] += arr[i - 1];
+        ArrayList<E> r = new ArrayList<>(size);
+        Class<? extends Number> e = (Class<? extends Number>) elementData[0].getClass();
+        for (int i = 0; i < size; i++) r.add((E) $.castNumber(arr[i], e));
+        return r;
     }
 
     /**
@@ -364,6 +508,27 @@ public class ArrayList<E> implements Iterable<E> {
     private void grow(int d) {
         if (d < 0) throw new IllegalArgumentException("Cannot shrink " + getClass().getSimpleName() + " capacity");
         else ensureCapacity(size += d);
+    }
+
+    /**
+     * Turns the list into a double[]. Note: T extends both {@code Number} and
+     * {@code E}, but I could not add both as bounds
+     * 
+     * @return the list as a double[]
+     * @throws ClassCastException if any of the elements is a different type from
+     *                            another
+     */
+    private <T extends E> double[] asDoubleArr() {
+        if (size == 0) return new double[0];
+        double[] prim = new double[size];
+        Class<T> t = (Class<T>) elementData[0].getClass();
+        if (t.isInterface()) throw new IllegalStateException("This should be impossible"); // This isn't in the javadoc because it should be impossible
+        if (!Number.class.isAssignableFrom(t)) throw new ClassCastException(t.getName() + " is not a Number");
+        for (int i = 0; i < size; i++) {
+            if (!elementData[i].getClass().equals(t)) throw new ClassCastException("Cannot add numbers of different types");
+            prim[i] += ((Number) get(i)).doubleValue();
+        }
+        return prim;
     }
 
     /**
