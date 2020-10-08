90666fb382eb0f9cd7b48a8cec75a27884d79a44
liambloom <liamrbloom@gmail.com>
2020-10-08 18:18:48 -0400

    Slight changes to StringInMatrix
    I made more variables final, removed deepToString (which was unnecesary), and renamed some methods to be more descriptive

--- src/io/github/liambloom/softwareEngineering/chapter11/StringInMatrix/SInM.java
+++ src/io/github/liambloom/softwareEngineering/chapter11/StringInMatrix/SInM.java
@@ -40,8 +40,8 @@ public class SInM {
         System.out.println("answer = NO");
     }
     public static void recursive() {
-        String s = "AABCDE";           
-        char M[][] = { 
+        final String s = "AABCDE";           
+        final char M[][] = { 
             {'A','B','A','A'}, 
             {'B','C','D','A'}, 
             {'A','B','C','B'}, 
@@ -51,14 +51,14 @@ public class SInM {
             {'E','D','C','B'}, 
             {'E','X','D','Z'} 
         };
-        if (!recursive_helper_top(M, s, new Point(0, 0))) 
+        if (!recursiveTop(M, s, new Point(0, 0))) 
             System.out.println("answer = NO");
     }
-    public static boolean recursive_helper_top(char[][] M, String s, Point startAt) {
+    public static boolean recursiveTop(final char[][] M, final String s, final Point startAt) {
         if (M[startAt.x][startAt.y] == s.charAt(0)) {
-            LinkedList<Point> coordinates = new LinkedList<>();
+            final LinkedList<Point> coordinates = new LinkedList<>();
             coordinates.add(startAt);
-            if (recursive_helper(M, s.substring(1), coordinates))
+            if (recursiveBranch(M, s.substring(1), coordinates))
                 return true;
         }
         startAt.transform(0, 1);
@@ -66,48 +66,33 @@ public class SInM {
             startAt.setLocation(startAt.x + 1, 0);
         if (startAt.x == M.length)
             return false;
-        return recursive_helper_top(M, s, startAt);
+        return recursiveTop(M, s, startAt);
     }
-    public static boolean recursive_helper(char[][] M, String s, LinkedList<Point> coordinates) {
+    public static boolean recursiveBranch(final char[][] M, final String s, final LinkedList<Point> coordinates) {
         if (s.length() == 0) {
             done(coordinates);
             return true;
         }
         // This is stored in a variable just for convenience, to make it shorter
-        Function<Point, Boolean> tryPoint = point -> tryPoint(M, s, coordinates, point);
+        final Function<Point, Boolean> tryPoint = point -> tryPoint(M, s, coordinates, point);
         final Point here = coordinates.getLast();
         return
+            // Direction check order is up, left, right, down
             tryPoint.apply(here.getTransformed(-1, 0)) ||
             tryPoint.apply(here.getTransformed(0, -1)) ||
             tryPoint.apply(here.getTransformed(0, 1)) || 
             tryPoint.apply(here.getTransformed(1, 0));
         
     }
-    public static boolean tryPoint(char[][] M, String s, LinkedList<Point> coordinates, Point point) {
+    public static boolean tryPoint(final char[][] M, final String s, final LinkedList<Point> coordinates, final Point point) {
         @SuppressWarnings("unchecked")
         final LinkedList<Point> clone = (LinkedList<Point>) coordinates.clone();
         return 0 <= point.x && point.x < M.length && 0 <= point.y && point.y < M[0].length &&
-            !coordinates.contains(point) && M[point.x][point.y] == s.charAt(0) && clone.add(point) && recursive_helper(M, s.substring(1), clone);
+            !coordinates.contains(point) && M[point.x][point.y] == s.charAt(0) && clone.add(point) && recursiveBranch(M, s.substring(1), clone);
     }
-    public static void done(LinkedList<Point> coordinates) {
+    public static void done(final LinkedList<Point> coordinates) {
         System.out.println("answer = YES");
         System.out.println("origin = " + coordinates.getFirst().toPointNotation());
         System.out.print("coordinates: " + coordinates);
-    }
-    public static String deepToString(Object o) {
-        if (Iterable.class.isAssignableFrom(o.getClass())) {
-            String s = "";
-            @SuppressWarnings("unchecked")
-            Iterator<Object> iter = ((Iterable<Object>) o).iterator();
-            if (iter.hasNext()) 
-                s += deepToString(iter.next());
-            while (iter.hasNext()) 
-                s += ", " + deepToString(iter.next());
-            return "[" + s + "]";
-        }
-        else if (o instanceof Point) 
-            return ((Point) o).toPointNotation();
-        else
-            return o.toString();
     }
 }
