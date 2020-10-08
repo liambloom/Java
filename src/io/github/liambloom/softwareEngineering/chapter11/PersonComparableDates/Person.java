7ad0cbf0d358368d76b63c30c16aef7eae0e9e40
liambloom <liamrbloom@gmail.com>
2020-10-08 18:15:41 -0400

    Added PersonComparableDates shell
    This is the given shell for a program that has Person objects that implement the Comparable interface, and are compared based on birthdate

---
+++ src/io/github/liambloom/softwareEngineering/chapter11/PersonComparableDates/Person.java
@@ -0,0 +1,33 @@
+package io.github.liambloom.softwareEngineering.chapter11.PersonComparableDates;
+
+/**
+ * Person_Comparable_Dates class does the following:
+ * 
+ * (1) implements "Comparable" and thus, YOU write the "compareTo()" method.
+ * Rem: public int compareTo(Object x)
+ * 
+ * (2) has the needed instance variables: myFirstName & myLastName of type
+ * String, myBirthDate of type Date (Compositioned inside of Person in other
+ * words, Person "hasA" Date) (3) has the needed constructors: default and
+ * regular that takes in 2 Strings for the name & 3 ints for the Date object.
+ * 
+ * (4) has getMyBirthDate() that returns the Person's myBirthDate.
+ * 
+ * (5) has a toString() method
+ * 
+ */
+public class Person implements Comparable<Person> {
+    public final String firstName;
+    public final String lastName;
+    public final Date birthdate;
+
+    public Person() {
+        // Date default is 0/0/0
+        // This doesn't exist
+        // That makes no sense
+    }
+
+    public Person(String first, String last, int month, int day, int year) {
+
+    }
+}
