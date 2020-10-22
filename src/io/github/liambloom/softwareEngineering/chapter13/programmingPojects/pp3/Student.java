0b71f6e87cabc4c69eedd9b31926f4ce89485c62
liambloom <liamrbloom@gmail.com>
2020-10-22 16:03:19 -0400

    Programming Project 3
    Also based off a shell. This uses comparators to sort by any feild. (Building Java programs : a back to basics approach / Stuart Reges, Marty Stepp--Third edition. page 868)

---
+++ src/io/github/liambloom/softwareEngineering/chapter13/programmingPojects/pp3/Student.java
@@ -0,0 +1,70 @@
+package io.github.liambloom.softwareEngineering.chapter13.programmingPojects.pp3;
+
+import java.util.Comparator;
+
+public class Student {
+    public String lastName, firstName;
+    public String idNum;
+    public Double average;
+    public char letterGrade;
+
+    public Student() {
+        this("", "", "", 0.0, ' ');
+    }
+
+    public Student(String lastName, String firstName, String idNum, double average, char letterGrade) {
+        this.lastName = lastName;
+        this.firstName = firstName;
+        this.idNum = idNum;
+        this.average = average;
+        this.letterGrade = letterGrade;
+    }
+
+    public String toString() {
+        return lastName + " " + firstName + " " + idNum + " " + average + " " + letterGrade;
+    }
+
+    public static class Comparators {
+        /*
+         * You said you wanted them sorted by five things, but the CLIENT
+         * code only sorts by 4 things, and you only listed 4 things (which
+         * were not the same 4 things the CLIENT sorted by). Since I don't
+         * know what you meant by "grade percentage," so I just went with 
+         * the original Programming Project 3, which was to be able to sort
+         * by each of the 5 fields. I hope that's what you wanted.
+         */
+
+        private Comparators () { }
+
+        public final static class LastName implements Comparator<Student> {
+            public int compare(Student s1, Student s2) {
+                return s1.lastName.compareTo(s2.lastName);
+            }
+        }
+
+        public final static class FirstName implements Comparator<Student> {
+            public int compare(Student s1, Student s2) {
+                return s1.firstName.compareTo(s2.firstName);
+            }
+        }
+
+        public final static class ID implements Comparator<Student> {
+            public int compare(Student s1, Student s2) {
+                return s1.idNum.compareTo(s2.idNum);
+            }
+        }
+
+        public final static class Average implements Comparator<Student> {
+            public int compare(Student s1, Student s2) {
+                final Double diff = s1.average - s2.average;
+                return diff > 0 ? 1 : diff < 0 ? -1 : 0;
+            }
+        }
+
+        public final static class LetterGrade implements Comparator<Student> {
+            public int compare(Student s1, Student s2) {
+                return s1.letterGrade - s2.letterGrade;
+            }
+        }
+    }
+} // Student
