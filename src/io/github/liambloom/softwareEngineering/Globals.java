091686d8e55bded5639e7000c6396ffc018a7ff1
liambloom <liamrbloom@gmail.com>
2020-11-09 09:36:12 -0500

    Did Ch. 8 PP. 1

--- src/io/github/liambloom/softwareEngineering/Globals.java
+++ src/io/github/liambloom/softwareEngineering/Globals.java
@@ -1,4 +1,5 @@
 package io.github.liambloom.softwareEngineering;
+import java.util.Random;
 
 public final class Globals {
     public static class Math {
@@ -30,6 +31,34 @@ public final class Globals {
                 max = java.lang.Math.max(max, nums[i]);
             }
             return max;
+        }
+
+        public static int gcd(int a, int b) {
+            return b == 0 ? a : gcd(b, a % b);
+        }
+
+        public static int lcm(int a, int b) {
+            return a * (b / gcd(a, b));
+        }
+
+        public static void main(String[] args) {
+            Random r = new Random();
+
+            for (int i = 0; i < 1000; i++) {
+                int[] ns = new int[2];
+                for (int j = 0; j < 2; j++)
+                    while ((ns[j] = r.nextInt(100) - 50) == 0);
+
+                final int lcm = lcm(ns[0], ns[1]);
+
+                System.out.printf("%d. %d, %d...", i, ns[0], ns[1]);
+                if (lcm % ns[0] == 0 && lcm % ns[1] == 0)
+                    System.out.println("\u001b[32mok\u001b[0m");
+                else {
+                    System.out.println("\u001b[31mfail\u001b[0m");
+                    System.exit(1);
+                }
+            }
         }
     }
     public static String repeat (String str, int repetitions) {
---
+++ src/io/github/liambloom/softwareEngineering/chapter8/programmingProjects/RationalNumber.java
@@ -0,0 +1,57 @@
+package io.github.liambloom.softwareEngineering.chapter8.programmingProjects;
+
+import io.github.liambloom.softwareEngineering.Globals;
+
+public class RationalNumber {
+
+    private final int numerator;
+    private final int denominator; // Should always be positive
+
+    public RationalNumber(int numerator, int denominator) {
+        if (denominator == 0)
+            throw new IllegalArgumentException();
+        final int gcd = Globals.Math.gcd(numerator *= Integer.signum(denominator), denominator = Math.abs(denominator));
+        this.numerator = numerator / gcd;
+        this.denominator = denominator / gcd;
+    }
+
+    public RationalNumber() {
+        this(0, 1);
+    }
+
+    public int getNumerator() {
+        return numerator;
+    }
+
+    public int getDenominator() {
+        return denominator;
+    }
+
+    // This is one of my favorite things about rust: 
+    // https://doc.rust-lang.org/std/ops/index.html
+    public RationalNumber add(RationalNumber o) {
+        final int lcm = Globals.Math.lcm(this.denominator, o.denominator);
+        return new RationalNumber(this.numerator * lcm / this.denominator + o.numerator * lcm / o.denominator, lcm);
+    }
+
+    public RationalNumber subtract(RationalNumber o) {
+        final int lcm = Globals.Math.lcm(this.denominator, o.denominator);
+        return new RationalNumber(this.numerator * lcm / this.denominator - o.numerator * lcm / o.denominator, lcm);
+    }
+
+    public RationalNumber multiply(RationalNumber o) {
+        return new RationalNumber(this.numerator * o.numerator, this.denominator * o.denominator);
+    }
+
+    public RationalNumber divide(RationalNumber o ) {
+        return new RationalNumber(this.numerator * o.denominator, this.denominator * o.numerator);
+    }
+
+    double getDecimalValue() {
+        return (double) numerator / denominator;
+    }
+
+    public String toString() {
+        return getDecimalValue() % 1 == 0 ? Integer.toString(numerator) : numerator + "/" + denominator;
+    }
+}
