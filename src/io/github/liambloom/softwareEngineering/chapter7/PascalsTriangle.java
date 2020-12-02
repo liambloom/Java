ff35009751611fb8a3d852c9f7c971b4da4a0b05
liambloom <liamrbloom@gmail.com>
2020-12-02 09:04:46 -0500

    Pascal 2

--- src/io/github/liambloom/softwareEngineering/chapter7/PascalsTriangle.java
+++ src/io/github/liambloom/softwareEngineering/chapter7/PascalsTriangle.java
@@ -1,6 +1,11 @@
 package io.github.liambloom.softwareEngineering.chapter7;
 
 public class PascalsTriangle {
+    public static void main(String[] args) {
+        method2(5);
+        method2(6);
+    }
+
     public static void method1(final int rows) {
         final int[][] matrix = new int[rows][rows * 2 - 1];
 
@@ -27,6 +32,43 @@ public class PascalsTriangle {
                 else
                     System.out.printf("%" + (length + 1) + "d", cell);
             }
+            System.out.println();
+        }
+    }
+
+    public static void method2(final int rows) {
+        final int[][] matrix = new int[rows][rows];
+
+        // fill 1s
+        for (int i = 0; i < rows; i++) {
+            matrix[i][0] = 1;
+            matrix[i][i] = 1;
+        }
+
+        // fill rest
+        for (int i = 2; i < rows; i++) {
+            for (int j = 1; j < i; j++) {
+                matrix[i][j] = matrix[i - 1][j] + matrix[i - 1][j - 1];
+            }
+        }
+
+        // used for both printouts
+        final int length = (int) Math.ceil(Math.log10(matrix[rows - 1][rows / 2] + 1)) + 1;
+        final String space = " ".repeat(length);
+
+        // print as stored
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j <= i; j++)
+                System.out.printf("%-" + length + "d", matrix[i][j]);
+            System.out.println();
+        }
+
+        // print pretty
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < rows - i - 1; j++)
+                System.out.print(space);
+            for (int j = 0; j <= i; j++)
+                System.out.printf("%" + length + "d" + space, matrix[i][j]);
             System.out.println();
         }
     }
