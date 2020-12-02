331eb2dd4db83fbccf921baf7f37af9dde52b056
liambloom <liamrbloom@gmail.com>
2020-12-02 13:16:54 -0500

    Pascal 4

--- src/io/github/liambloom/softwareEngineering/chapter7/PascalsTriangle.java
+++ src/io/github/liambloom/softwareEngineering/chapter7/PascalsTriangle.java
@@ -1,11 +1,6 @@
 package io.github.liambloom.softwareEngineering.chapter7;
 
 public class PascalsTriangle {
-    public static void main(String[] args) {
-        method2(5);
-        method2(6);
-    }
-
     public static void method1(final int rows) {
         final int[][] matrix = new int[rows][rows * 2 - 1];
 
@@ -67,8 +62,7 @@ public class PascalsTriangle {
 
         // print pretty
         for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < rows - i - 1; j++)
-                System.out.print(space);
+            System.out.print(space.repeat(rows - i - 1));
             for (int j = 0; j <= i; j++)
                 System.out.printf("%" + length + "d" + space, matrix[i][j]);
             System.out.println();
@@ -76,7 +70,6 @@ public class PascalsTriangle {
     }
 
     public static void method3(final int rows) {
-        final int cols = (rows + 1) / 2;
         final int[][] matrix = new int[rows][];
 
         // fill 1s
@@ -96,7 +89,7 @@ public class PascalsTriangle {
         }
 
         // used for both printouts
-        final int length = (int) Math.ceil(Math.log10(matrix[rows - 1][cols - 1] + 1)) + 1;
+        final int length = (int) Math.ceil(Math.log10(matrix[rows - 1][(rows + 1) / 2 - 1] + 1)) + 1;
         final String space = " ".repeat(length);
 
         // print as stored
@@ -108,14 +101,61 @@ public class PascalsTriangle {
 
         // print pretty
         for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < rows - i - 1; j++)
-                System.out.print(space);
+            System.out.print(space.repeat(rows - i - 1));
             for (int j = 0; j < (i + 1) / 2; j++)
                 System.out.printf("%" + length + "d" + space, matrix[i][j]);
             if (i % 2 == 0)
                 System.out.printf("%" + length + "d" + space, matrix[i][i / 2]);
             for (int j = (i + 1) / 2 - 1; j >= 0; j--)
                 System.out.printf("%" + length + "d" + space, matrix[i][j]);
+            System.out.println();
+        }
+    }
+
+    public static void method4(final int rows) {
+        final int[][] matrix = new int[rows - 2][];
+
+        // fill 1st column
+        for (int i = 0; i < matrix.length; i++) {
+            matrix[i] = new int[i / 2 + 1];
+            matrix[i][0] = i + 2;
+        }
+
+        // fill rest
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 1; j < i / 2 + 1; j++) {
+                if (j == (i + 1) / 2)
+                    matrix[i][j] = 2 * matrix[i - 1][j - 1];
+                else
+                    matrix[i][j] = matrix[i - 1][j] + matrix[i - 1][j - 1];
+            }
+        }
+        
+        // used for both printouts
+        final int length = (int) Math.ceil(Math.log10(matrix[matrix.length - 1][(matrix.length - 1) / 2] + 1)) + 1;
+        final String space = " ".repeat(length);
+
+        // print as stored
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < i / 2 + 1; j++)
+                System.out.printf("%-" + length + "d", matrix[i][j]);
+            System.out.println();
+        }
+
+        // print pretty
+        for (int i = -2; i < matrix.length; i++) {
+            System.out.print(space.repeat(matrix.length - i - 1));
+            System.out.printf("%" + length + "d" + space, 1);
+            if (i >= 0) {
+                for (int j = 0; j < (i + 1) / 2; j++)
+                    System.out.printf("%" + length + "d" + space, matrix[i][j]);
+                if (i % 2 == 0)
+                    System.out.printf("%" + length + "d" + space, matrix[i][i / 2]);
+                for (int j = (i + 1) / 2 - 1; j >= 0; j--)
+                    System.out.printf("%" + length + "d" + space, matrix[i][j]);
+            }
+            if (i != -2)
+                System.out.printf("%" + length + "d" + space, 1);
             System.out.println();
         }
     }
