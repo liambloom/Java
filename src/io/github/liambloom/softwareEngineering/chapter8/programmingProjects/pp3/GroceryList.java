40224d0d80ab54fca7503be6d1856e967e4ad9a9
liambloom <liamrbloom@gmail.com>
2020-12-22 09:03:56 -0500

    Did chatper 8 pp 3

---
+++ src/io/github/liambloom/softwareEngineering/chapter8/programmingProjects/pp3/GroceryList.java
@@ -0,0 +1,19 @@
+package io.github.liambloom.softwareEngineering.chapter8.programmingProjects.pp3;
+
+public class GroceryList {
+    GroceryItemOrder[] items = new GroceryItemOrder[10];
+    int listLength = 0;
+
+    public void add(GroceryItemOrder item) {
+        // The book doesn't say what to do if there's more than 10 items
+        items[listLength] = item;
+        listLength++;
+    }
+
+    public double getTotalCost() {
+        double cost = 0;
+        for (int i = 0; i < listLength; i++)
+            cost += items[i].getCost();
+        return cost;
+    }
+}
