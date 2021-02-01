b3f96c14173c5984e760697ab68ea28d4beb94e8
liambloom <liamrbloom@gmail.com>
2021-02-01 17:42:49 -0500

    Started Chater 17

---
+++ src/io/github/liambloom/softwareEngineering/chapter17/intTree/Client.java
@@ -0,0 +1,17 @@
+package io.github.liambloom.softwareEngineering.chapter17.intTree;
+
+// Short program that demonstrates the use of the IntTree class.
+// BJP 3rd Edition pg 1020-1023
+
+public class Client {
+    public static void main(String[] args) {
+        IntTree t = new IntTree(12);
+
+        System.out.println("Tree structure on its side:");
+        t.printSideways();
+        System.out.println();
+        t.printPreorder();
+        t.printInorder();
+        t.printPostorder();
+    }
+}
