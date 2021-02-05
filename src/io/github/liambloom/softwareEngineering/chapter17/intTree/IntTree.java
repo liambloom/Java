54fa49c80bec71bc524f529ae3e20d321de906ef
liambloom <liamrbloom@gmail.com>
2021-02-05 09:53:46 -0500

    Exercises 1-5

--- src/io/github/liambloom/softwareEngineering/chapter17/intTree/IntTree.java
+++ src/io/github/liambloom/softwareEngineering/chapter17/intTree/IntTree.java
@@ -8,8 +8,34 @@ package io.github.liambloom.softwareEngineering.chapter17.intTree;
 // documentation refers to these as "sequential trees."
 // BJP 3rd Edition pg 1020-1023
 
+import io.github.liambloom.tests.Tester;
+
 public class IntTree {
+public static final IntTree ref1 = new IntTree(new IntTreeNode(3, new IntTreeNode(5, new IntTreeNode(1), null),
+        new IntTreeNode(2, new IntTreeNode(4), new IntTreeNode(6))));
+public static final IntTree ref2 = new IntTree(
+        new IntTreeNode(2, new IntTreeNode(8, new IntTreeNode(0), null), new IntTreeNode(1,
+                new IntTreeNode(7, new IntTreeNode(4), null), new IntTreeNode(6, null, new IntTreeNode(9)))));
+public static final IntTree ref3 = new IntTree(
+        new IntTreeNode(2, new IntTreeNode(3, new IntTreeNode(8), new IntTreeNode(7)), new IntTreeNode(1)));
+
+    public static void main(String[] args) {
+        Tester tester = new Tester(Tester.Policy.RunLast);
+        tester
+            .test(ref1::countLeftNodes, 3)
+            .test(ref1::countEmpty, 7)
+            .test(ref1::depthSum, 50)
+            .test(ref2::countEvenBranches, 3)
+            .testOutput(() -> ref2.printLevel(3), "0\n7\n6\n");
+
+        tester.close();
+    }
+
     private IntTreeNode overallRoot;
+
+    private IntTree(IntTreeNode root) {
+        overallRoot = root;
+    }
 
     public IntTree(int max) {
         if (max <= 0) {
@@ -96,5 +122,61 @@ public class IntTree {
             System.out.println(root.data);
             printSideways(root.left, level + 1);
         }
+    }
+
+    // Exercise 1
+    public int countLeftNodes() {
+        return countLeftNodes(overallRoot);
+    }
+
+    private int countLeftNodes(IntTreeNode root) {
+        return root == null ? 0 : 1 + countLeftNodes(root.left);
+    }
+
+    // Exercise 2
+    public int countEmpty() {
+        return countEmpty(overallRoot);
+    }
+
+    private int countEmpty(IntTreeNode root) {
+        return root == null ? 1 : countEmpty(root.left) + countEmpty(root.right);
+    }
+
+    // Exercise 3
+    public int depthSum() {
+        return depthSum(overallRoot, 1);
+    }
+
+    private int depthSum(IntTreeNode root, int level) {
+        return root == null ? 0 : root.data * level + depthSum(root.left, level + 1) + depthSum(root.right, level + 1);
+    }
+
+    // Exercise 4
+    public int countEvenBranches() {
+        return countEvenBranches(overallRoot);
+    }
+
+    private int countEvenBranches(IntTreeNode root) {
+        return root == null || root.left == null && root.right == null 
+            ? 0 
+            : (root.data + 1) % 2 + countEvenBranches(root.left) + countEvenBranches(root.right);
+    }
+
+    // Exercise 5
+    public void printLevel(int level) {
+        if (level < 1)
+            throw new IllegalArgumentException();
+        printLevel(overallRoot, 1, level);
+    }
+
+    private void printLevel(IntTreeNode root, int currentLevel, int targetLevel) {
+        if (root == null)
+            return;
+        if (currentLevel < targetLevel) {
+            printLevel(root.left, currentLevel + 1, targetLevel);
+            printLevel(root.right, currentLevel + 1, targetLevel);
+        }
+        else if (currentLevel == targetLevel)
+            System.out.println(root.data);
     }
 }
