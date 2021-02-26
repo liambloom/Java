640ef522a1df3cf33eecd17a77327567a2ebfa54
liambloom <liamrbloom@gmail.com>
2021-02-26 08:53:25 -0500

    Balance tree

---
+++ src/io/github/liambloom/softwareEngineering/chapter17/balance/TreeNode.java
@@ -0,0 +1,15 @@
+package io.github.liambloom.softwareEngineering.chapter17.balance;
+
+class TreeNode<T> {
+    TreeNode<T> left, right;
+    T data;
+
+    public TreeNode(final T data) {
+        this.data = data;
+    }
+
+    @Override
+    public String toString() {
+        return data.toString();
+    }
+}
